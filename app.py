ab1d9cb667c1a87bf13fcb8d1552665a6c1fe7ec
Zigelzi <40773205+Zigelzi@users.noreply.github.com>
2019-04-25 21:50:37 +0300

    Added first links and basic routing

--- app.py
+++ app.py
@@ -4,3 +4,14 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/vuosikello")
+def vuosikello():
+    return render_template('vuosikello.html')
+
+@app.route("/yhteystiedot")
+def yhteystiedot():
+    return render_template('yhteystiedot.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
