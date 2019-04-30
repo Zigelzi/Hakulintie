d5aa2839892e4f7c3240876986d5f5ccd0ed2bfe
Zigelzi <40773205+Zigelzi@users.noreply.github.com>
2019-04-30 13:34:16 +0300

    Added DB and updated registeration form

--- forms.py
+++ forms.py
@@ -4,13 +4,19 @@ from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 
 class Rekisteroidy(FlaskForm):
+
+    first_name = StringField('Etunimi', validators=[DataRequired()])
+    last_name = StringField('Sukunimi', validators=[DataRequired()])
+    house =  StringField('Asunto',
+                         validators=[DataRequired(),
+                                     Length(max=3, message='Talon numerossa voi olla vain kolme merkkikä')])
     email = StringField('Sähköposti',
-                            validators=[DataRequired(), Email(message="Ei voimassa oleva sähköposti")])
+                        validators=[DataRequired(), Email(message="Ei voimassa oleva sähköposti")])
     password = PasswordField('Salasana',
-                            validators=[DataRequired(),
-                            Length(min=8, message="Salasanan täytyy olla vähintään 8 merkkiä pitkä")])
+                             validators=[DataRequired(),
+                                         Length(min=8, message="Salasanan täytyy olla vähintään 8 merkkiä pitkä")])
     confirm_pw = PasswordField('Vahvista salasana',
-                            validators= [DataRequired(), EqualTo('password', message="Salasana ei täsmää")])
+                                validators= [DataRequired(), EqualTo('password', message="Salasana ei täsmää")])
     submit = SubmitField('Luo tunnus')
     
 
