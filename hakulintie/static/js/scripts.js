ec407dabc9da463f09cfa96c57400d5de0564d29
zigelzi <miika.savela@gmail.com>
2019-05-08 21:06:46 +0300

    Added .js comments and removed .inline-form from modal

--- hakulintie/static/js/scripts.js
+++ hakulintie/static/js/scripts.js
@@ -2,10 +2,12 @@ var modal = document.querySelector('.modal')
 var trigger = document.querySelector('.btn-danger')
 var closeButton = document.querySelector('.close-button')
 
+// Toggle the modal css property.
 function toggleModal() {
     modal.classList.toggle('show-modal');
 }
 
+// Close the modal when clicked off the window
 function windowOnClick(event) {
     if (event.target === modal) {
         toggleModal()
